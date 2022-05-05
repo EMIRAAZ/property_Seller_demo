ac3d66a7447839b3fafd8f161f940a3e1ee06497
vishnudeviravi <ivishnudr@gmail.com>
2022-05-05 18:49:44 +0530

    ADDING HEADER : creating header

--- src/components/button/HeaderButton/index.jsx
+++ src/components/button/HeaderButton/index.jsx
@@ -1,7 +1,23 @@
+import { useLocation, useSearchParams } from 'react-router-dom';
 import './headerButton.scss';
 
 const HeaderButton = ({ children }) => {
-  return <button className="header-btn">{children}</button>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const search = useLocation().search;
+
+  const onLanguageChange = () => {
+    const lang = new URLSearchParams(search).get('lang');
+
+    if (lang === 'eng') setSearchParams({ lang: 'ar' });
+    else if (lang === 'ar') setSearchParams({ lang: 'eng' });
+    else setSearchParams({ lang: 'ar' });
+  };
+
+  return (
+    <button onClick={onLanguageChange} className="header-btn">
+      {children}
+    </button>
+  );
 };
 
 export default HeaderButton;
---
+++ src/components/svg/downarrow/index.jsx
@@ -0,0 +1,19 @@
+const DownArrow = () => {
+  return (
+    <div>
+      <svg
+        width="8"
+        height="6"
+        viewBox="0 0 8 6"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M0.886719 0.181641L4.10468 5.20937L7.32263 0.181641H0.886719Z"
+          fill="#02101C"
+        />
+      </svg>
+    </div>
+  );
+};
+export default DownArrow;
