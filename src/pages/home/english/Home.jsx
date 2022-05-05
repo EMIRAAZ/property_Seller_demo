26e99b8d63ffb6ac26d1b93c192da3aaea7d6c81
vishnudeviravi <ivishnudr@gmail.com>
2022-05-06 01:13:43 +0530

    MARGIN_FIX: added margin fix in public file

--- src/pages/home/english/Home.jsx
+++ src/pages/home/english/Home.jsx
@@ -1,7 +1,12 @@
 import './home.scss';
+import Header from '../../../components/header';
 
 const Home = () => {
-  return <div className="home-english">english</div>;
+  return (
+    <div className="home-english">
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
