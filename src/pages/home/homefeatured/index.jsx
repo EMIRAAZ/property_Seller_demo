0208bd18c23eb6ba67ec34b22d7816c5ac477922
vishnudeviravi <ivishnudr@gmail.com>
2022-05-13 00:08:07 +0530

    FEATURED ADDED

--- src/pages/home/homefeatured/index.jsx
+++ src/pages/home/homefeatured/index.jsx
@@ -1,9 +1,21 @@
 import './homefeatured.scss';
+import LeftNavigate from '../../../components/svg/leftnavigate';
+import RightNavigate from '../../../components/svg/rightnavigate';
+import ProgressLine from './progressline';
+import FeaturedList from './featuredlist';
 
 const HomeFeatured = () => {
   return (
     <div className="home-featured">
-      <div className="heading">Featured</div>
+      <div className="header">
+        <p>Featured</p>
+        <div className="navigate">
+          <LeftNavigate />
+          <RightNavigate />
+        </div>
+      </div>
+      <ProgressLine />
+      <FeaturedList />
     </div>
   );
 };
