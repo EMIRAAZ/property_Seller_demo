62a65dc1e69651dc21dae922761fc9b668e3297c
vishnudeviravi <ivishnudr@gmail.com>
2022-05-26 22:25:22 +0530

    HOME PAGE PROPERTY BUG FIX

--- src/pages/home/homeProperty/index.jsx
+++ src/pages/home/homeProperty/index.jsx
@@ -4,11 +4,21 @@ import Property from '../../../components/property';
 const HomeProperty = ({ property }) => {
   const renderProperty = () =>
     property.property.map(item => <Property key={item.id} {...item} />);
+
+  const renderPropertyHeader = () => {
+    if (property.property && property.property.length > 0)
+      return (
+        <>
+          <p className="heading">Properties</p>
+          <p className="sub">{property.count} Properties found</p>
+        </>
+      );
+    else return null;
+  };
   return (
     <div className="home-property">
       <div className="home-property-content">
-        <p className="heading">Properties</p>
-        <p className="sub">{property.count} Properties found</p>
+        {renderPropertyHeader()}
         <div className="property-list-container">{renderProperty()}</div>
       </div>
     </div>
