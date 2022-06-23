14a1321cd251ef4cbde5b4755d5282fed243a35d
leobahul <b.bahul007@gmail.com>
2022-06-23 09:18:46 +0530

    NEWS: news list page added

--- src/pages/news/english/News.jsx
+++ src/pages/news/english/News.jsx
@@ -1,7 +1,50 @@
+import Footer from "../../../components/footer";
+import Header from "../../../components/header";
 import "./news.scss";
 
 const News = () => {
-  return <div></div>;
+  return (
+    <div className="news-main-container">
+      <Header />
+
+      <div className="container">
+        <h1 className="heading">
+          Samsung Galaxy F22 hshslaunched in India: Price, features, other
+          details
+        </h1>
+        <div className="category">
+          <div className="item-div">
+            <h1 className="item">Tech</h1>
+          </div>
+          <div className="item-div">
+            <h1 className="item">Mobile</h1>
+          </div>
+        </div>
+        <h1 className="sub-heading">
+          Samsung's next Unpacked event reortedly last August 11
+        </h1>
+        <img src="/assets/image/blog6.jpg" className="image-div" alt="" />
+
+        <p className="news-content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+          veritatis minus dolor aspernatur enim velit! Cum voluptates vero
+          ducimus molestiae reiciendis animi esse alias nesciunt consectetur,
+          nihil architecto sit tempora? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Unde, enim numquam. Expedita officia iusto quas
+          laborum praesentium rem repellat nulla culpa dolores ut, provident
+          accusantium. Sint debitis delectus consequuntur doloremque. <br />{" "}
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, illo
+          qui eveniet impedit atque dolore, quisquam, iure earum expedita natus
+          facilis! Pariatur doloremque nesciunt alias cupiditate dolorum
+          voluptate ullam dicta? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis, aperiam accusamus? Quas voluptatum earum
+          repellendus non vitae ut dignissimos eum inventore laborum tenetur.
+          Laborum laudantium, adipisci dolor facere cupiditate at?{" "}
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default News;
--- src/preview/bahul/index.jsx
+++ src/preview/bahul/index.jsx
@@ -1,9 +1,10 @@
 import Footer from "../../components/footer/english/Footer";
 import OffplanView from "../../pages/offplanview/english/OffplanView";
+import Topstory from "../../pages/topstory/english/Topstory";
 import "./index.scss";
 
 const Preview = () => {
-  return <OffplanView />;
+  return <Topstory />;
   // return null;
 };
 
