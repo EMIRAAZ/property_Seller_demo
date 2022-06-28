4683a963809023bc62c80472176d1d62ebd1c14a
vishnudeviravi <ivishnudr@gmail.com>
2022-06-28 12:56:53 +0530

    BUY PAGINATION

--- src/pages/buy/english/Buy.jsx
+++ src/pages/buy/english/Buy.jsx
@@ -6,13 +6,28 @@ import BuyFormCard from '../buyformcard';
 import BuyProperty from '../buyproperty';
 import Pagination from '../../../components/pagination';
 import Footer from '../../../components/footer/english/Footer';
+import { makeUrlParam } from '../../../utils';
 
 const Buy = props => {
   useEffect(() => {
     props.getBuyProperty();
   }, []);
 
-  const onChangeCurrentPage = current => {};
+  const onChangeCurrentPage = current => {
+    const offset = 10 * current - 10 > 0 ? 10 * current - 10 : 0;
+    const newParamInput = { ...props.paramInput };
+    if (typeof newParamInput.location === 'object') {
+      const inputWithoutLocation = {
+        ...newParamInput,
+        ...newParamInput.location,
+      };
+      delete inputWithoutLocation.location;
+      props.getProperty(
+        `&${makeUrlParam(inputWithoutLocation)}&offset=${offset}`
+      );
+    } else
+      props.getProperty(`&${makeUrlParam(newParamInput)}&offset=${offset}`);
+  };
   return (
     <div className="buy-english">
       <Header />
