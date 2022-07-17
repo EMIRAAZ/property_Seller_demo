d3127989a1e4ad0f3dd6544354c641ba9d7dcafa
vishnudeviravi <ivishnudr@gmail.com>
2022-07-17 13:11:29 +0530

    REMOVED UNWANTED IMAGES

--- src/pages/home/imageframe/index.jsx
+++ src/pages/home/imageframe/index.jsx
@@ -1,5 +1,4 @@
 import './imageFrame.scss';
-import RightArrow from '../../../components/svg/rightarrow';
 
 const ImageFrame = () => {
   return (
@@ -7,46 +6,6 @@ const ImageFrame = () => {
       <p className="home-properties-caption">
         Your Assistant for Properties in <span> &nbsp;UAE</span>
       </p>
-      {/* <img
-        className="img-main-home"
-        src="/assets/image/home-main2.jpg"
-        alt="building"
-      /> */}
-      {/* <div className="background-square"></div>
-      <div className="main-div">
-        <div className="text">
-          <p className="first"> Properties</p>
-          <p className="second">Lets Find Your Properties in UAE</p>
-          <p className="third">
-            Rent, Buy and sell amazing private properties across Dubai.
-          </p>
-          <div className="home-black-btn">
-            <RightArrow
-              customClass="right-arrow-rotate"
-              width="25"
-              height="20"
-              viewBox="4 -6 16 14"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="img-div">
-        <img src="/assets/image/build1.jpg" alt="building" />
-      </div>
-      <div className="img-div no-mobile">
-        <img
-          className="desktop-only"
-          src="/assets/image/build2.jpg"
-          alt="building"
-        />
-      </div>
-      <div className="img-div no-mobile">
-        <img
-          className="desktop-only"
-          src="/assets/image/build5.jpg"
-          alt="building"
-        />
-      </div> */}
     </div>
   );
 };
--- src/pages/neighbourhood/imageframe/index.jsx
+++ src/pages/neighbourhood/imageframe/index.jsx
@@ -1,5 +1,4 @@
 import './imageFrame.scss';
-import RightArrow from '../../../components/svg/rightarrow';
 
 const ImageFrame = () => {
   return (
@@ -12,41 +11,6 @@ const ImageFrame = () => {
         src="/assets/image/neighbourhood-main.jpg"
         alt="building"
       />
-      {/* <div className="background-square"></div>
-      <div className="main-div">
-        <div className="text">
-          <p className="first"> Properties</p>
-          <p className="second">Lets Find Your Properties in UAE</p>
-          <p className="third">
-            Rent, Buy and sell amazing private properties across Dubai.
-          </p>
-          <div className="neighbourhood-black-btn">
-            <RightArrow
-              customClass="right-arrow-rotate"
-              width="25"
-              height="20"
-              viewBox="4 -6 16 14"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="img-div">
-        <img src="/assets/image/build1.jpg" alt="building" />
-      </div>
-      <div className="img-div no-mobile">
-        <img
-          className="desktop-only"
-          src="/assets/image/build2.jpg"
-          alt="building"
-        />
-      </div>
-      <div className="img-div no-mobile">
-        <img
-          className="desktop-only"
-          src="/assets/image/build5.jpg"
-          alt="building"
-        />
-      </div> */}
     </div>
   );
 };
