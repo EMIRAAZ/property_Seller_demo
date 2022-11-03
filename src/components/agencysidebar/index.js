ae1c7d4218010194ae328c333d441391ea4d0b45
leobahul <b.bahul007@gmail.com>
2022-11-03 11:11:41 +0530

    agency:abcd

--- src/components/agencysidebar/index.js
+++ src/components/agencysidebar/index.js
@@ -1,32 +1,33 @@
-import './agencysidebar.scss';
-import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import Dashboard from '../svg/dashboard';
-import Agent from '../svg/agent';
-import Logout from '../svg/logout';
-import ListAgent from '../svg/listagent';
-import AddProperty from '../svg/addproperty';
+import "./agencysidebar.scss";
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import Dashboard from "../svg/dashboard";
+import Agent from "../svg/agent";
+import Logout from "../svg/logout";
+import ListAgent from "../svg/listagent";
+import AddProperty from "../svg/addproperty";
 
 const first = [
-  { name: 'Dashboard', link: '/agency', logo: Dashboard },
+  { name: "Dashboard", link: "/agency", logo: Dashboard },
   // { name: 'List Agent', link: '/agency/list-agent', logo: ListAgent },
-  { name: 'Add Agent', link: '/agency/add-agent', logo: Agent },
-  { name: 'Add Property', link: '/agency/add-property', logo: AddProperty },
+  { name: "Add Agent", link: "/agency/add-agent", logo: Agent },
+  { name: "Add Property", link: "/agency/add-property", logo: AddProperty },
+  { name: " List of Agents", link: "/agency/list-agent", logo: Agent },
 ];
-const second = [{ name: 'Log Out', link: '', logo: Logout }];
+const second = [{ name: "Log Out", link: "", logo: Logout }];
 
 const AgencySidebar = () => {
   return (
     <div className="agency-sidebar">
       <p className="agency-sidebar-header">UAE Assistant</p>
       <ul className="first-list">
-        {first.map(item => (
+        {first.map((item) => (
           <RenderSingleList {...item} />
         ))}
       </ul>
       <hr />
       <ul className="second-list">
-        {second.map(item => (
+        {second.map((item) => (
           <RenderSingleList {...item} />
         ))}
       </ul>
@@ -37,11 +38,11 @@ const AgencySidebar = () => {
 const RenderSingleList = ({ name, link, logo }) => {
   let navigate = useNavigate();
   let location = useLocation();
-  const getLink = () => location.pathname.split('/').pop();
+  const getLink = () => location.pathname.split("/").pop();
   const onClick = () => {
-    if (name === 'Log Out') {
+    if (name === "Log Out") {
       localStorage.clear();
-      navigate('/agency/login');
+      navigate("/agency/login");
     } else {
       navigate(link);
     }
@@ -49,10 +50,10 @@ const RenderSingleList = ({ name, link, logo }) => {
 
   return (
     <li
-      className={getLink() === link.split('/').pop() ? 'active-li' : ''}
+      className={getLink() === link.split("/").pop() ? "active-li" : ""}
       onClick={onClick}
     >
-      {logo ? React.createElement(logo, { className: 'logo-sidebar' }) : null}
+      {logo ? React.createElement(logo, { className: "logo-sidebar" }) : null}
       <p> {name}</p>
     </li>
   );
