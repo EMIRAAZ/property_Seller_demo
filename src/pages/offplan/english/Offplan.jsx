c91a6f76c4e3fe3b178fe1b606a60ab218418e8e
harikriz22 <harikriz50@gmail.com>
2022-11-17 09:36:07 +0530

    OFFPLAN MAIN PAGE

--- src/pages/offplan/english/Offplan.jsx
+++ src/pages/offplan/english/Offplan.jsx
@@ -7,36 +7,135 @@ import OffplanCard from "../../../components/offpaln/offplanCard/OffplanCard";
 import { MoveToTop } from "../../../components/movetotop";
 import FooterNew from "../../../components/footerNew";
 
-const Offplan = (props) => {
-  const renderOffplan = () =>
-    props.offplan.data.map((item) => (
-      <div className="offplan-card-size-div">
-        <OffplanCard key={item.id} {...item} />
-      </div>
-    ));
-  return (
-    <>
-      <div className="offplan-main-container-1">
-        <Header customClass="offplan-header-class" />
-        <div className="offplan-card-container"></div>
-        <ListHeading
-          main="Offplan Properties"
-          count={props.offplan.count}
-          sub="offplans found"
-        />
-        <RenderComponent
-          type="OFFPLAN"
-          data={props.offplan.data}
-          propertyCallApi={props.getOffplanWeb}
-          count={props.offplan.count}
-          iQuery={`&limit=${6}&offset=${0}`}
-          isPagination
-        />
-        <MoveToTop />
-        <FooterNew />
-      </div>
-    </>
-  );
-};
+// const Offplan = (props) => {
+//   const renderOffplan = () =>
+//     props.offplan.data.map((item) => (
+//       <div className="offplan-card-size-div">
+//         <OffplanCard key={item.id} {...item} />
+//       </div>
+//     ));
+//   return (
+//     <>
+//       <div className="offplan-main-container-1">
+//         <Header customClass="offplan-header-class" />
+//         <div className="offplan-card-container"></div>
+//         <ListHeading
+//           main="Offplan Properties"
+//           count={props.offplan.count}
+//           sub="offplans found"
+//         />
+//         <RenderComponent
+//           type="OFFPLAN"
+//           data={props.offplan.data}
+//           propertyCallApi={props.getOffplanWeb}
+//           count={props.offplan.count}
+//           iQuery={`&limit=${6}&offset=${0}`}
+//           isPagination
+//         />
+//         <MoveToTop />
+//         <FooterNew />
+//       </div>
+//     </>
+//   );
+// };
 
+// export default Offplan;
+const offplancontents=[  {
+  heading: "Golden View Dubai",
+  address: "4 BHK Appartment",
+  position:'DownTown, Dubai',
+  description:
+    " There are many variations of passages of lorem Ipsum available, but the majority have suffered alteration in some form injected. There are many variations of passages of lorem Ipsum available",
+  image: "/assets/image/gift1.jpg",
+},
+{
+  heading: "Golden View Dubai",
+  address: "4 BHK Appartment",
+  position:'DownTown, Dubai',
+  description:
+    " There are many variations of passages of lorem Ipsum available, but the majority have suffered alteration in some form injected. There are many variations of passages of lorem Ipsum available",
+  image: "/assets/image/gift1.jpg",
+},
+{
+  heading: "Golden View Dubai",
+  address: "4 BHK Appartment",
+  position:'DownTown, Dubai',
+  description:
+    " There are many variations of passages of lorem Ipsum available, but the majority have suffered alteration in some form injected. There are many variations of passages of lorem Ipsum available",
+  image: "/assets/image/gift1.jpg",
+},
+{
+  heading: "Golden View Dubai",
+  address: "4 BHK Appartment",
+  position:'DownTown, Dubai',
+  description:
+    " There are many variations of passages of lorem Ipsum available, but the majority have suffered alteration in some form injected. There are many variations of passages of lorem Ipsum available",
+  image: "/assets/image/gift1.jpg",
+},
+{
+  heading: "Golden View Dubai",
+  address: "4 BHK Appartment",
+  position:'DownTown, Dubai',
+  description:
+    " There are many variations of passages of lorem Ipsum available, but the majority have suffered alteration in some form injected. There are many variations of passages of lorem Ipsum available",
+  image: "/assets/image/gift1.jpg",
+},
+{
+  heading: "Golden View Dubai",
+  address: "4 BHK Appartment",
+  position:'DownTown, Dubai',
+  description:
+    " There are many variations of passages of lorem Ipsum available, but the majority have suffered alteration in some form injected. There are many variations of passages of lorem Ipsum available",
+  image: "/assets/image/gift1.jpg",
+}
+
+
+
+
+,]
+
+const Offplan =()=>{
+
+
+  const renderoffplan = () =>
+  offplancontents.map((item, i) => <OffplanCard key={i} {...item} />);
+  
+  return(
+    <div className="main-off-container">
+      <Header/>
+      <ListHeading
+        className="list-header"
+        main="Appartments"
+        count={3587}
+      />
+      {/* <p className="off-found">3587 Properties Found</p> */}
+      <div className="categories">
+          <div className='item-div'>
+            <h1 className="item-name">ALL</h1>
+          </div>
+            <div className='item-div'>
+              <h1 className="item-name">Studio</h1>
+            </div>
+            <div className='item-div'>
+              <h1 className="item-name">1 Bed room</h1>
+            </div>
+            <div className='item-div'>
+              <h1 className="item-name">2 Bed room</h1>
+            </div>
+     
+        </div>
+    <div className="offplan-container">
+    <RenderComponent
+        data={offplancontents}
+        propertyCallApi={()=>{}}
+        count={2}
+        type='OFFPLAN'
+        iQuery={`limit=${6}&offset=${0}`}
+        isPagination
+      />
+    </div>
+    <Footer/>
+    </div>
+  )
+}
 export default Offplan;
--- src/preview/hari/index.jsx
+++ src/preview/hari/index.jsx
@@ -3,11 +3,15 @@ import "./index.scss";
 import Nft from "../../pages/nft/english/Nft";
 
 import Policy from "../../pages/privacypolicy/english/Policy";
+import Offplan from "../../pages/offplan/english/Offplan";
+import News from "../../pages/news";
+import OffplanCard from "../../components/offpaln/offplanCard/OffplanCard";
+import OffplanView from "../../pages/offplanview/english/OffplanView";
 
 const Preview = () => {
   return (
     <>
-      <Policy />
+      <Offplan />
     </>
   );
 };
