297a8d0b48837a9f1ee8432833bd48c4126d78a8
vishnudeviravi <ivishnudr@gmail.com>
2022-11-25 19:11:21 +0530

    ADDING CHANGES

--- src/redux/reducers/offplan/index.js
+++ src/redux/reducers/offplan/index.js
@@ -6,6 +6,12 @@ import {
   GET_OFFPLAN_BY_ID,
   GET_OFFPLAN_BY_ID_STARTED,
   GET_OFFPLAN_BY_ID_ERROR,
+  GET_OFFPLAN_PROJECTS_WEB,
+  GET_OFFPLAN_PROJECTS_WEB_STARTED,
+  GET_OFFPLAN_PROJECTS_WEB_ERROR,
+  GET_OFFPLAN_PROJECTS_BY_ID,
+  GET_OFFPLAN_PROJECTS_BY_ID_STARTED,
+  GET_OFFPLAN_PROJECTS_BY_ID_ERROR,
 } from '../../constants';
 
 const reducer = (state = initialState, action) => {
@@ -63,6 +69,63 @@ const reducer = (state = initialState, action) => {
         ...state,
         singleOffplan: {
           ...state.singleOffplan,
+          error: true,
+          loading: false,
+        },
+      };
+    case GET_OFFPLAN_PROJECTS_WEB:
+      return {
+        ...state,
+        offplanProjects: {
+          ...state.offplanProjects,
+          error: false,
+          loading: false,
+          data: action.payload.rows,
+          count: action.payload.count,
+        },
+      };
+    case GET_OFFPLAN_PROJECTS_WEB_STARTED:
+      return {
+        ...state,
+        offplanProjects: {
+          ...state.offplanProjects,
+          error: false,
+          loading: true,
+        },
+      };
+    case GET_OFFPLAN_PROJECTS_WEB_ERROR:
+      return {
+        ...state,
+        offplanProjects: {
+          ...state.offplanProjects,
+          error: true,
+          loading: false,
+        },
+      };
+    case GET_OFFPLAN_PROJECTS_BY_ID:
+      return {
+        ...state,
+        singleOffplanProjects: {
+          ...state.singleOffplanProjects,
+          error: false,
+          loading: false,
+          offplanProjects: action.payload,
+        },
+      };
+    case GET_OFFPLAN_PROJECTS_BY_ID_STARTED:
+      return {
+        ...state,
+        singleOffplanProjects: {
+          ...state.singleOffplanProjects,
+          error: false,
+          loading: true,
+        },
+      };
+    case GET_OFFPLAN_PROJECTS_BY_ID_ERROR:
+      return {
+        ...state,
+        singleOffplanProjects: {
+          ...state.singleOffplanProjects,
           error: true,
           loading: false,
         },
