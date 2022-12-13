c839b91c6ae0ed976c15d8e030a29d28f13afe28
vishnudeviravi <ivishnudr@gmail.com>
2022-12-13 22:53:42 +0530

    ADDING CHANGES

--- src/pages/home/imageframe/index.jsx
+++ src/pages/home/imageframe/index.jsx
@@ -1,12 +1,14 @@
 import './imageFrame.scss';
 
 const ImageFrame = props => {
+  const adjustSizeWithScreen = () => {
+    if (window.innerHeight < 640) {
+      return 'ls-home-img-frame';
+    }
+    return 'gt-home-img-frame';
+  };
   return (
-    <div
-      className={`home-image-frame ${
-        props.advancedSearch ? 'ad-s-on-frame' : ''
-      }`}
-    >
+    <div className={`home-image-frame ${adjustSizeWithScreen()}`}>
       <p className="home-properties-caption">Find Properties for Sale</p>
     </div>
   );
