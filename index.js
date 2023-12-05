cd836af41286cfe50846f020db83ea136ed062d5
MernNihad <mernnihadsoft@gmail.com>
2023-12-06 01:40:34 +0530

    change api address to domain name with https name

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
-app.get('/j', (req, res) => {
-  res.send('Hello World!')
-})
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle all routes and serve the main index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-const path = require('path')
-app.use('/', express.static(path.join(__dirname, 'build')))
+  console.log(`Server listening on port ${port}`);
+});
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: 'http://54.163.214.5/',
+  // baseURL: 'http://54.163.214.5/',
+  baseURL: 'https://propertyassistant.ae/',
+
   timeout: 20000,
 });
