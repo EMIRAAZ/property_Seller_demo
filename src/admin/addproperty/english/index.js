18636fbdf7acd7c95c29c4719229ac6877fd83ce
EMIRAAZ <emiraaztechnologies@gmail.com>
2023-12-18 17:46:51 +0530

    change commit nad push

--- src/admin/addproperty/english/index.js
+++ src/admin/addproperty/english/index.js
@@ -31,8 +31,7 @@ const mapDispatchToProps = dispatch => {
   return {
     onInputChange: payload => dispatch(changeAdminPropertyInput(payload)),
     addAdminProperty: (payload, cb) => dispatch(addAdminProperty(payload, cb)),
-    editAdminProperty: (id, payload, cb) =>
-      dispatch(editAdminProperty(id, payload, cb)),
+    editAdminProperty: (id, payload, cb) => dispatch(editAdminProperty(id, payload, cb)),
     getAgentProperty: payload => dispatch(getAgentProperty(payload)),
     getTaglineProperty: payload => dispatch(getTaglineProperty(payload)),
     getNeighborhoodProperty: payload =>
